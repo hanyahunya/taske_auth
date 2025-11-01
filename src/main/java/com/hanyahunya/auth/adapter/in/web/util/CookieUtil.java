315e08c3053a372346be1642569c93e09abf18a1
hanyahunya <java020103@gmail.com>
2025-11-01 23:38:17 +0900

    chore(config): デプロイ環境向けに application.yml を設定

--- src/main/java/com/hanyahunya/auth/adapter/in/web/util/CookieUtil.java
+++ src/main/java/com/hanyahunya/auth/adapter/in/web/util/CookieUtil.java
@@ -5,11 +5,32 @@ import org.springframework.http.ResponseCookie;
 public final class CookieUtil {
     private CookieUtil() {}
 
+    public static ResponseCookie createRefreshTokenCookie(String refreshToken) {
+        return ResponseCookie.from("refresh_token", refreshToken)
+                .httpOnly(true)
+                .secure(true)
+                .path("/taske")
+                .domain("hanyahunya.com")
+                .maxAge(15 * 24 * 60 * 60)
+                .sameSite("Lax")
+                .build();
+    }
+
+    public static ResponseCookie deleteRefreshTokenCookie() {
+        return ResponseCookie.from("refresh_token", "")
+                .httpOnly(true)
+                .secure(true)
+                .path("/taske")
+                .domain("hanyahunya.com")
+                .maxAge(0)
+                .build();
+    }
+
 //    public static ResponseCookie createRefreshTokenCookie(String refreshToken) {
 //        return ResponseCookie.from("refresh_token", refreshToken)
 //                .httpOnly(true)
-//                .secure(true) // https
-//                .path("/taske") // only taske
+//                .secure(false)
+//                .path("/")
 //                .maxAge(15 * 24 * 60 * 60)
 //                .sameSite("Lax")
 //                .build();
@@ -18,24 +39,7 @@ public final class CookieUtil {
 //    public static ResponseCookie deleteRefreshTokenCookie() {
 //        return ResponseCookie.from("refresh_token", "")
 //                .maxAge(0)
-//                .path("/taske")
+//                .path("/")
 //                .build();
 //    }
-
-    public static ResponseCookie createRefreshTokenCookie(String refreshToken) {
-        return ResponseCookie.from("refresh_token", refreshToken)
-                .httpOnly(true)
-                .secure(false)
-                .path("/")
-                .maxAge(15 * 24 * 60 * 60)
-                .sameSite("Lax")
-                .build();
-    }
-
-    public static ResponseCookie deleteRefreshTokenCookie() {
-        return ResponseCookie.from("refresh_token", "")
-                .maxAge(0)
-                .path("/")
-                .build();
-    }
 }
